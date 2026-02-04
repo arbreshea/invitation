78efcd4e7262f11710780b727cc7e8376fc4f98b
Arbreshe <ahmetiarbreshe@gmail.com>
2026-02-04 09:26:14 +0100

    change color

--- src/components/layout/MobileShell.tsx
+++ src/components/layout/MobileShell.tsx
@@ -6,7 +6,7 @@ type MobileShellProps = {
 
 export default function MobileShell({ children }: MobileShellProps) {
   return (
-    <div className="min-h-dvh pb-20 bg-white text-black">
+    <div className="min-h-dvh pb-20 bg-rose-50 text-black">
       {/* This centers a phone-sized canvas on desktop, but fills screen on phones */}
       <div className="mx-auto min-h-dvh w-full max-w-107.5">{children}</div>
     </div>
